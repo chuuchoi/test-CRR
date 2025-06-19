1be6827fb7fe7242b90c8f59b38df32c52846b74
chuuchoi <51234226+chuuchoi@users.noreply.github.com>
2025-06-19 18:28:14 +0900

    fix

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,5 +4,5 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false, //★GitHub Pages에 배포하는 경우 정적 배포로, SSR false
-  basename:"/test-CRR" // ★GitHub Pages 서브폴더일 경우 반드시 깃헙 리포지토리 이름 필요
+  basename:"/test-CRR/" // ★GitHub Pages 서브폴더일 경우 반드시 깃헙 리포지토리 이름 필요
 } satisfies Config;
